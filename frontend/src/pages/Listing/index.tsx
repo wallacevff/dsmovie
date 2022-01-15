6c123b316f968881a912be1b69fb36014c61c569
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2022-01-15 19:08:21 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,25 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
 
-  // FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-      console.log(response.data);
+      setPageNumber(data.number);
     });
+  }, []);
+
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">

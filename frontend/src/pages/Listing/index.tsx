8987d47e15eb6d4ab59a2a91ca8adcf00122775e
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2022-01-15 21:05:19 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,9 +29,13 @@ function Listing() {
       });
   }, [pageNumber]);
 
+  const handlePageChange = (newpageNumber : number) => {
+    setPageNumber(newpageNumber);
+  }
+
   return (
     <>
-      <Pagination />
+      <Pagination  page = {page} onChange={handlePageChange}/>
       <div className="container">
         <div className="row">
           {page.content.map((movie) => (
